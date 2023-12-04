a2df61e2c6971678e5532840a1131c1655759138
Austin de Coup-Crank <adecoupcrank@tesla.com>
2023-12-04 11:25:34 -0600

    Arrow functions

---
+++ 01-fundamentals/13-arrow-functions.js
@@ -0,0 +1,10 @@
+
+// There is another concise syntax for creating functions:
+
+let sum = (a, b) => +a + +b;
+
+// This function adds a and b, converting to integer as necessary
+
+console.log(sum("1", "2")) // 3
+
+// Arrow functions are mostly used for one-liners and callbacks
