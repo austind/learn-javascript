db02018e97a4e4d7c3a1b0a05dd1f8143ca8a9f4
Austin de Coup-Crank <adecoupcrank@tesla.com>
2024-01-04 11:04:15 -0600

    object conversion

--- 04-objects/07-object-conversion.js
+++ 04-objects/07-object-conversion.js
@@ -12,7 +12,7 @@
 // What happens in ambiguous cases?
 
 let total = obj1 + obj2;
-if (user == 1) { ... };
+if (user == 1) { /* ... */ };
 
 // Both of these cases use the "default" case.
 
@@ -32,4 +32,30 @@ alert(user); // hint: string => {name: "John"}
 alert(+user); // hint: number => 1000
 alert(user + 500); // hint: default => 1500
 
-// Left off: https://javascript.info/object-toprimitive#tostring-valueof
+// toString and valueOf are old ways of hinting.
+// They must return a primitive value. If they return an object, they're ignored.
+
+user = {name: "John"};
+alert(user); // [object Object]
+alert(user.valueOf() === user); // true
+
+/*
+The important thing to know about all primitive-conversion methods is that they do
+not necessarily return the “hinted” primitive.
+
+There is no control whether toString returns exactly a string, or whether
+Symbol.toPrimitive method returns a number for the hint "number".
+
+The only mandatory thing: these methods must return a primitive, not an object.
+*/
+
+let obj = {
+    // toString handles all conversions in the absence of other methods
+    toString() {
+        return "2";
+    }
+}
+
+alert(obj * 2); // 4, object converted to string "2", then multiplication made it an integer
+alert(obj + 2); // "22", binary plus operator converts to string if any operand is a string
+
