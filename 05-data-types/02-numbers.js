015bf16d5bc3fabd4d2a9b7eea286799e0d8ca75
Austin de Coup-Crank <adecoupcrank@tesla.com>
2024-01-04 14:59:21 -0600

    More numbers

--- 05-data-types/02-numbers.js
+++ 05-data-types/02-numbers.js
@@ -23,4 +23,36 @@ let a = 0b11111111; // Binary form of 255
 let b = 0o377; // Octal form of 255
 alert( a == b ); // true, same number
 
-// Left off: https://javascript.info/number#tostring-base
+// Numbers have a built-in method toString(base)
+let num = 255;
+alert( num.toString(16) ); // "ff"
+alert( num.toString(2) ); // "11111111"
+
+// Two dots call a method on a number directly, e.g.:
+123456..toString(); // "123456"
+
+// The Math function provides rounding
+console.log(Math.floor(3.14)); // 3
+console.log(Math.ceil(3.14)); // 4
+console.log(Math.round(3.14)); // 3
+console.log(Math.trunc(3.14)); // 3; removes everything after decimal with no rounding
+
+// Rounding to a given decimal place can be done in two ways:
+num = 1.23456;
+console.log(Math.round(num * 100) / 100); // 1.23
+console.log(num.toFixed(1)); // "1.23" (note, this is a string, not a number)
+
+// Numbers that exceed the 64-bit size become a special numeric value Infinity
+console.log(1e500); // Infinity
+
+// There's some unexpected side-effects of the number system in place:
+console.log(0.1 + 0.2 == 0.3); // false
+console.log(0.1 + 0.2); // 0.30000000000000004
+
+// This is due to the way binary can't store certain numbers exactly, and rounds
+// them to the nearest number. The way around this is using toFixed, optionally
+// casted back to Number with unary plus:
+let sum = 0.1 + 0.2;
+console.log(+sum.toFixed(2)); // 0.3
+
+// https://javascript.info/number#tests-isfinite-and-isnan
