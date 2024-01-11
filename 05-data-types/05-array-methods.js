e872493ff512b27610584a9976f957192da4b87e
Austin de Coup-Crank <adecoupcrank@tesla.com>
2024-01-11 10:17:57 -0600

    Begin array methods

--- 05-data-types/05-array-methods.js
+++ 05-data-types/05-array-methods.js
@@ -1,3 +1,22 @@
 
 // https://javascript.info/array-methods
 
+// Splice is the Swiss army knife of arrays.
+// NOTE: Negative indices ARE allowed.
+
+// Delete elements
+let arr = ["I", "like", "Python"];
+arr.splice(1, 1); // from index 1, remove 1 element
+// Note that splice returns any deleted elements
+
+// Replace elements
+arr = ["I", "want", "to", "study", "JavaScript"];
+arr.splice(3, 2, "go", "hiking");
+console.log(arr); // I,want,to,go,hiking
+
+// Insert elements
+// Set deleteCount (second argument) to 0
+arr = ["I", "study", "JavaScript"];
+arr.splice(2, 0, "complex", "language");
+console.log(arr); // "I", "study", "complex", "language", "JavaScript"
+
