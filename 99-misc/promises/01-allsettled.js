e7e2afc1a9f3bd51a074a888a4636d26ff30e7bf
Austin de Coup-Crank <adecoupcrank@tesla.com>
2024-02-05 08:54:51 -0600

    Promises exercise

---
+++ 99-misc/promises/01-allsettled.js
@@ -0,0 +1,15 @@
+
+const promise1 = new Promise((resolve, reject) => setTimeout(resolve, 1000, 'Promise 1 resolved'));
+const promise2 = new Promise((resolve, reject) => setTimeout(reject, 890, 'Promise 2 rejected'));
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 1200, 'Promise 3 resolved'));
+
+Promise.allSettled([promise1, promise2, promise3])
+  .then(results => {
+    results.forEach(result=> {
+      if (result.status === 'fulfilled') {
+        console.log(`Fulfilled: ${result.value}`);
+      } else {
+        console.log(`Rejected: ${result.reason}`)
+      }
+    })
+  });
